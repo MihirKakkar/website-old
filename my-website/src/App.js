8c4606d9b97c5d63cde23f4dc62fea16171461cb
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-05-04 18:18:07 -0400

    complete change to website

---
+++ my-website/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    
+    <div id="part_with_title" className="App">
+      <script src="https://kit.fontawesome.com/959728ec6b.js" crossorigin="anonymous"></script>
+      <heading className="App-header">
+
+        <h1>Mihir Kakkar</h1>
+        <p>Nanotechnology Engineering @ UWaterloo</p>
+
+        <div id="part_with_icons">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+
+          <a href='https://github.com/MihirKakkar' target="_blank">
+            <i class="fa fa-github"></i>
+          </a>
+          <a href='https://www.linkedin.com/in/mihir-kakkar/' target="_blank">
+            <i class="fa fa-linkedin"></i>
+          </a>
+          <a href="mailto:m4kakkar@uwaterloo.ca" target="_blank">
+            <i class="fa fa-envelope"></i>
+          </a>
+          <a href="https://drive.google.com/open?id=1ZdQFSViq6Vi6fhVnsZagbJpcDScUzPLW" target="_blank">
+            <i class="fa fa-file"></i>
+          </a>
+        </div>
+      </heading>
+      
+        <section id="section02"  class="demo">
+        <a href="#section02"><span></span>:D</a></section>
+      <body className="App-body">
+        <h2>Hey there, I'm Mihir 👋</h2>
+        <p2>I'm an engineering student with a passion for making new things! </p2>
+        <p3></p3>
+      </body>
+    </div>
+  );
+}
+
+export default App;
