b932befd350e2d1aaa5f5962fdb762f041acb478
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-05-04 21:57:25 -0400

    adding more

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -31,12 +31,15 @@ function App() {
       </heading>
       
         <section id="section02"  class="demo">
-        <a href="#section02"><span></span>:D</a></section>
+        <a href="#section03"><span></span>:D</a></section>
       <body className="App-body">
         <h2>Hey there, I'm Mihir 👋</h2>
-        <p2>I'm an engineering student with a passion for making new things! </p2>
-        <p3></p3>
+        <p2>I'm an engineering student with a passion for making new things.</p2>
+        <p2>On the side, I love basketball, cooking, and my guitar!</p2>
+        <p2>Currently I am looking for 8-month internship opportunities starting September 2020.</p2>
+        <p2>Feel free to reach out!</p2>
       </body>
+      <p3 id="section03">Made with 💖 by Mihir 2020</p3>
     </div>
   );
 }
